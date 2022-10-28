c4354edcd3cd70930a88ba51faed346c260cdbf8
Yogita Bhargava <79744488+yogitabhargava02@users.noreply.github.com>
2022-10-28 13:49:11 +0530

    first

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b0c2038476msh5eafbb3d1ee5b77p1accc4jsn6d571ee650a7',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions";
+
